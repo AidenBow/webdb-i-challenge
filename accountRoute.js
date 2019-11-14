730574ac8c95c4e5b5dd9bfdbad9bf9d5be8e1a0
AidenBow <AidenBow@users.noreply.github.com>
2019-11-13 19:43:59 -0800

    post, put, and delete accounts

--- accountRoute.js
+++ accountRoute.js
@@ -3,8 +3,38 @@ const router = express.Router();
 
 const db = require('./data/dbConfig.js');
 
+router.post("/", (req, res) => {
+  db("accounts").insert({name: req.body.name, budget: req.body.budget})
+    .then(accounts => {
+      res.status(200).json(accounts)
+    })
+    .catch(err => {
+      res.status(500).json({message: "server error", error: err.message})
+    })
+})
+
 router.get("/", (req, res) => {
-  db.select("*").from("accounts")
+  db("accounts").select("*")
+    .then(accounts => {
+      res.status(200).json(accounts)
+    })
+    .catch(err => {
+      res.status(500).json({message: "server error", error: err.message})
+    })
+})
+
+router.put("/:id", (req, res) => {
+  db("accounts").where( 'id', '=', req.params.id).update({name: req.body.name, budget: req.body.budget})
+    .then(accounts => {
+      res.status(200).json(accounts)
+    })
+    .catch(err => {
+      res.status(500).json({message: "server error", error: err.message})
+    })
+})
+
+router.delete("/:id", (req, res) => {
+  db("accounts").where("id", "=", req.params.id).del()
     .then(accounts => {
       res.status(200).json(accounts)
     })
